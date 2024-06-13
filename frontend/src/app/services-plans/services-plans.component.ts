26e77b847aa6ddacefc4f08861abd50dccd57860
alexxcalle <axcalle@sudamericano.edu.ec>
2024-06-12 23:06:47 -0500

    add ngzorro and finished

--- frontend/src/app/services-plans/services-plans.component.ts
+++ frontend/src/app/services-plans/services-plans.component.ts
@@ -4,12 +4,10 @@ import { Component } from '@angular/core';
 import { OnInit } from '@angular/core';
 import { NzTableModule } from 'ng-zorro-antd/table';
 import { NzButtonModule } from 'ng-zorro-antd/button';
-
 @Component({
   selector: 'app-services-plans',
   standalone: true,
-  imports: [CommonModule, HttpClientModule, NzTableModule, NzButtonModule
-  ],
+  imports: [CommonModule, HttpClientModule, NzTableModule, NzButtonModule, ],
   templateUrl: './services-plans.component.html',
   styleUrl: './services-plans.component.css',
   host: {
@@ -18,6 +16,7 @@ import { NzButtonModule } from 'ng-zorro-antd/button';
 
 export class ServicesPlansComponent implements OnInit{
   public data: any[] = [];
+  public displayedColumns: string[] = [];
   constructor(private httpClient: HttpClient) {}
 
   ngOnInit(): void {
@@ -26,10 +25,13 @@ export class ServicesPlansComponent implements OnInit{
 
   fetchData(): void {
     this.httpClient
-      .get('http://localhost:3000/api')
+      .get('http://localhost:3000/services-plans')
       .subscribe((data: any) => {
         console.log(data);
         this.data = data;
+        if(this.data.length > 0){
+          this.displayedColumns = Object.keys(this.data[0]);
+        }
       });
   }
 
@@ -43,9 +45,11 @@ export class ServicesPlansComponent implements OnInit{
 
   patch(id: number): void {
     this.httpClient
-      .patch(`http://localhost:3000/api/${id}`, { name: 'new name' })
+      .patch(`http://localhost:3000/services-plans/${id}`, { name: 'new name' })
       .subscribe(() => {
         this.fetchData();
       });
   }
+
+
 }
